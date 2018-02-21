0684fac2afd7704aa880b3331f97fbf4b961e749
Sebass13 <sebikele@gmail.com>
2018-02-21 01:04:50 -0600

    Allow for multiple dong-checks at the same time
    The most important part of having a penis is to compare. Previously, users would have to go through the arduous process of typing `[p] <user>` many times to see how they stack up in the server, but now they can do so all in one line.

--- penis/penis.py
+++ penis/penis.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from __main__ import send_cmd_help
 
 class Penis:
     """Penis related commands."""
@@ -8,16 +9,25 @@ class Penis:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def penis(self, *, user : discord.Member):
+    @commands.command(pass_context=True)
+    async def penis(self, ctx, *users : discord.Member):
         """Detects user's penis length
 
         This is 100% accurate."""
+        if len(users) == 0:
+            await send_cmd_help(ctx)
+            return
+        
         state = random.getstate()
-        random.seed(user.id)
-        dong = "8{}D".format("=" * random.randint(0, 30))
+        user_dongs = []
+        longest = max(len(user.name) for user in users) # So we can neatly line up the dongs
+        for user in users:          
+            random.seed(user.id)
+            dong = "8{}D".format("=" * random.randint(0, 30))
+            user_dongs.append("Size of {1:<{0}} {2}".format(longest + 1, user.name + ":", dong))
         random.setstate(state)
-        await self.bot.say("Size: " + dong)
+        
+        await self.bot.say("\n".join(user_dongs))
 
 
 def setup(bot):
